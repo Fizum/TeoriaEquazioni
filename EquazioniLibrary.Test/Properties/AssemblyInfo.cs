80e26b025049d019d6a9367cc5a05f504c6829a4
ITT Rosselli Rasetti <rosselli.rasetti@outlook.com>
2019-01-30 09:13:10 +0100

    Creazione di Test per la verifica di un'equazione determinata

---
+++ EquazioniLibrary.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("EquazioniLibrary.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("EquazioniLibrary.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("da9760cb-5fe2-4c18-898f-a3241fcc0355")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ EquazioniLibrary.Test/UnitTest1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using EquazioniLibrary;
+
+namespace EquazioniLibrary.Test
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestDet1()
+        {
+            double a = 3;
+            bool respeq = true;
+            bool resp = Equazioni.IsDeterminated(a);
+
+            Assert.AreEqual(respeq, resp);
+        }
+        public void TestDet2()
+        {
+            double a = 3;
+            double b = 0;
+            bool respeq = true;
+            bool resp = Equazioni.IsDeterminated(a);
+
+            Assert.AreEqual(respeq, resp);
+
+        }
+    }
+}
